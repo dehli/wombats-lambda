5bb3aa5d07340a54ae89c7f0cbeaf3871c5b15e5
Christian Paul Dehli <dehli@gatech.edu>
2017-02-02 17:57:35 -0500

    moved base64 stuff to api.

--- clojure/project.clj
+++ clojure/project.clj
@@ -2,6 +2,5 @@
   :description "Used to run a wombat's code through Amazon lambda"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [base64-clj "0.1.1"]
                  [uswitch/lambada "0.1.2"]]
   :aot :all)
--- clojure/src/wombats.clj
+++ clojure/src/wombats.clj
@@ -1,18 +1,12 @@
 (ns wombats
   (:require [uswitch.lambada.core :refer [deflambdafn]]
             [clojure.data.json :as json]
-            [clojure.java.io :as io]
-            [base64-clj.core :as b64]))
-
-(defn- decode-bot
-  [content]
-  (let [base64-string (clojure.string/replace content #"\n" "")]
-    (b64/decode base64-string "UTF-8")))
+            [clojure.java.io :as io]))
 
 (defn handle-event
   [event time-left]
   (let [state (get event "state")
-        code-string (decode-bot (get event "code"))]
+        code-string (get event "code")]
     ;; Call the wombat's code, passing in appropriate args
     (prn code-string)
     (try
