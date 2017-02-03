71a7431d62801311f1af88df642682c1c635d6ec
Christian Paul Dehli <dehli@gatech.edu>
2017-02-02 19:27:57 -0500

    Switched to using keywords.

--- clojure/project.clj
+++ clojure/project.clj
@@ -2,5 +2,6 @@
   :description "Used to run a wombat's code through Amazon lambda"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
+                 ;;[cheshire "5.7.0"]
                  [uswitch/lambada "0.1.2"]]
   :aot :all)
--- clojure/src/wombats.clj
+++ clojure/src/wombats.clj
@@ -1,22 +1,23 @@
 (ns wombats
   (:require [uswitch.lambada.core :refer [deflambdafn]]
+            ;;[cheshire.core :as cheshire]
             [clojure.data.json :as json]
             [clojure.java.io :as io]))
 
 (defn handle-event
   [event time-left]
-  (let [state (get event "state")
-        code-string (get event "code")]
-    ;; Call the wombat's code, passing in appropriate args
-    (try
-      ((load-string code-string) state time-left)
-      (catch Exception e))))
+  ;; Call the passed in code with the appropriate args
+  (try
+    ((load-string (:code event)) (:state event) time-left)
+    (catch Exception e)))
 
 (deflambdafn wombats.Handler
   [in out ctx]
   (let [time-left (fn [] (.getRemainingTimeInMillis ctx))
-        event (json/read (io/reader in))
+        event (json/read (io/reader in) :key-fn keyword)
+        ;;event (cheshire/parse-stream (io/reader in) true)
         res (handle-event event time-left)]
 
     (with-open [w (io/writer out)]
+      ;;(cheshire/generate-stream res w))))
       (json/write res w))))
